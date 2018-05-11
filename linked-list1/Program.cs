917db5bc77face8b8d388f928d987787a5377269
Janesh <jzd0009@tigermail.auburn.edu>
2018-05-11 10:28:25 -0700

    Add project files.

---
+++ linked-list1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace linked_list1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            List<string> list = new List<string>;
+            list.Add("hello");
+            */
+
+            LinkedList list = new LinkedList();
+
+            /*
+            Console.WriteLine("is it empty? - " + list.Empty);
+            Console.WriteLine("size - " + list.Count);
+            Console.ReadLine();
+            */
+
+            //int index = list.IndexOf("hello");
+
+            /*
+            list.Add("Test1");
+
+            int index = list.IndexOf("Test1");
+
+            bool contains = list.Contains("Test1");
+            list.Add("Test2");
+            list.Add(1, "Test3");
+            list.Add(1, "Test4");
+
+            list.Remove(1);
+            //list.Clear();
+            */
+
+            list.Add("Hello");
+            list.Add("Test1");
+            list.Add("Test2");
+
+            object test = list[2];
+        }
+    }
+}
---
+++ linked-list1/node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked_list1
+{
+    public class Node
+    {
+        /* COnstructor
+         * [x] Node(object data, Node next)
+         * 
+         * Private Fields :
+         * [x]object data - contain the data of the node, what we want to store in the list
+         * [x]Node next  - reference to the next node in the list
+         * 
+         * Public Properties:
+         * [x] Data - get / set the data field 
+         * [x] next - get / set the next field 
+         */
+
+        private object data;
+        private Node next;
+
+        public Node(object data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public object Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
